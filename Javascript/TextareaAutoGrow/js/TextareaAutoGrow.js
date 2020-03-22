5e5d22354e7f4e72acc990bc6cc70de9890994ba
theobenoit <theo.benoit16@gmail.com>
2020-03-22 22:31:08 +0100

    Textarea Autogrow

---
+++ Javascript/TextareaAutoGrow/js/TextareaAutoGrow.js
@@ -0,0 +1,60 @@
+/**
+ * Call callback function after a certain delay
+ *
+ * @param callback
+ * @param delay
+ * @returns {function(...[*]=)}
+ */
+const debounce = (callback, delay) => {
+    let timer;
+    return function () {
+        let args = arguments,
+            context = this;
+
+        clearTimeout(timer);
+        timer = setTimeout(() => callback.apply(context, args), delay);
+    }
+};
+
+/**
+ * Manage textarea auto growing when user type, resize, focus the textarea
+ */
+class TextareaAutoGrow extends HTMLTextAreaElement {
+
+    constructor() {
+        super();
+        this.onResize = debounce(this.onResize.bind(this), 300);
+    }
+
+    connectedCallback() {
+        this.addEventListener('focus', this.onFocus);
+    }
+
+    disconnectedCallback() {
+        window.removeEventListener('resize', this.onResize);
+    }
+
+    onFocus = () => {
+        this.style.overflow = 'hidden';
+        this.style.resize = 'none';
+        this.style.boxSizing = 'border-box';
+
+        this.autoGrow();
+
+        window.addEventListener('resize', this.onResize);
+        this.addEventListener('input', this.autoGrow);
+        this.removeEventListener('focus', this.onFocus);
+    };
+
+    onResize() {
+        this.autoGrow();
+    };
+
+    autoGrow = () => {
+        this.style.height = 'auto';
+        this.style.height = `${this.scrollHeight}px`;
+    };
+
+}
+
+customElements.define('textarea-autogrow', TextareaAutoGrow, {extends: 'textarea'});
