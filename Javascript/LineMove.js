1db4de0ed406a669d3481b7ac63bc8ba772d2722
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2020-06-18 12:46:15 +0200

    Create LineMove.js

---
+++ Javascript/LineMove.js
@@ -0,0 +1,183 @@
+const debounce = (callback, delay) => {
+    let timer;
+    return function () {
+        let args = arguments,
+            context = this;
+
+        clearTimeout(timer);
+        timer = setTimeout(() => callback.apply(context, args), delay);
+    }
+};
+
+const throttle = (callback, delay) => {
+    let last,
+        timer;
+
+    return function () {
+        let context = this,
+            now = +new Date(),
+            args = arguments;
+
+        if (last && now < last + delay) {
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                last = now;
+                callback.apply(context, args);
+            }, delay);
+        } else {
+            last = now;
+            callback.apply(context, args);
+        }
+    };
+};
+
+/**
+ * Line moving on cursor hover
+ */
+export default class LineMove
+{
+
+    constructor(settings = {})
+    {
+        this.settings = {
+            element: '',
+            hover: '',
+            activeSelector: '',
+            debug: false
+        };
+        this._setupSettings(settings);
+
+        if (!this.settings.element) throw new Error('You need to specify element selector for LineMove to work correctly.');
+        if (!this.settings.hover) throw new Error('You need to specify hover selector for LineMove to work correctly.');
+
+        this._cacheDOM();
+
+        this._init();
+    }
+
+    /**
+     * Cache DOM elements
+     *
+     * @private
+     */
+    _cacheDOM()
+    {
+        this.$el = document.querySelector(this.settings.element);
+        this.$activeItem = document.querySelector(this.settings.activeSelector);
+        this.$hovers = document.querySelectorAll(this.settings.hover);
+    }
+
+    /**
+     * Set up settings defined in the creation of the instance
+     *
+     * @param settings
+     * @private
+     */
+    _setupSettings(settings)
+    {
+        if (Object.keys(settings).length > 0) {
+            for (let key in settings) {
+                if (settings.hasOwnProperty(key) && this.settings.hasOwnProperty(key)) this.settings[key] = settings[key];
+            }
+        }
+    }
+
+    /**
+     * Move the line to an element
+     *
+     * @param $element
+     * @param resize
+     */
+    moveTo($element, resize = false)
+    {
+        if (
+            typeof $element === 'undefined' ||
+            $element === null ||
+            $element.length === 0 ||
+            window.getComputedStyle(this.$el).display === 'none' ||
+            this.$el.offsetParent === null
+        ) return;
+
+        if (this.settings.debug) {
+            console.info(
+                `%c Line\n%o \n has moved to \n %o.`,
+                'background: #222; color: #bada55',
+                this.$el,
+                $element
+            );
+        }
+
+        const elementRect = $element.getBoundingClientRect();
+        const isLineActive = this.$el.getAttribute('line-is-active');
+
+        // No transition if we don't see the line
+        if (!isLineActive) {
+            this.$el.setAttribute('line-is-active', 'true');
+            this.$el.style.transition = 'none';
+        } else {
+            this.$el.style.transition = '';
+        }
+
+        this.$el.style.width = `${elementRect.width}px`;
+        this.$el.style.left = `${elementRect.left}px`;
+
+        if (!resize) {
+            if (this.$previousActiveLineItem) this.$previousActiveLineItem.removeAttribute('line-item-is-active');
+            $element.setAttribute('line-item-is-active', 'true');
+            this.$previousActiveLineItem = $element;
+            this.$activeLineItem = $element;
+        }
+    }
+
+    /**
+     * Move the line to the active item on load
+     *
+     * @private
+     */
+    _moveLineOnLoad()
+    {
+        this.moveTo(this.$activeItem);
+    }
+
+    /**
+     * Handle hover on line
+     *
+     * @private
+     */
+    _handleHover()
+    {
+        const that = this;
+
+        this.$hovers.forEach($hover => {
+            $hover.addEventListener('mouseover', throttle(function () {
+                that.moveTo(this);
+            }, 25));
+            $hover.addEventListener('mouseout', throttle(function () {
+                that.moveTo(that.$activeItem);
+            }, 25));
+        });
+    }
+
+    /**
+     * Move the line on resize
+     *
+     * @private
+     */
+    _onResize = () =>
+    {
+        this.moveTo(this.$activeLineItem, true);
+    };
+
+    /**
+     * Initialization of everything
+     *
+     * @private
+     */
+    _init()
+    {
+        this._moveLineOnLoad();
+        this._handleHover();
+        window.addEventListener('resize', debounce(this._onResize, 50));
+    }
+
+}
