4874b1f1b4ad1d003632b633f2ecc40f4cca3ed6
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2020-08-13 10:22:48 +0200

    Create Menu.js

---
+++ Javascript/Menu/Menu.js
@@ -0,0 +1,85 @@
+import MenuItem from './MenuItem';
+
+/**
+ * Handle menu
+ */
+export default class Menu
+{
+
+    constructor()
+    {
+        this.menu = [];
+    }
+
+    /**
+     * Add an item to the menu
+     *
+     * @param menuItem
+     */
+    addItem(menuItem)
+    {
+        if (!menuItem instanceof MenuItem) throw new Error('You need to add an instance of MenuItem in the method addItem of the Menu');
+        this.menu.push(menuItem);
+    }
+
+    /**
+     * Get the menu
+     *
+     * @returns {[]|*[]}
+     */
+    getMenu()
+    {
+        return this.menu;
+    }
+
+    /**
+     * Close all sub menus
+     */
+    closeAllSubMenus()
+    {
+        this.getMenu().forEach(menuItem => {
+           if (menuItem.haveSubMenu() && menuItem.subMenuIsOpened()) menuItem.closeSubMenuAndDescendant();
+        });
+    }
+
+    /**
+     * Get length of menu
+     *
+     * @returns {number}
+     */
+    getLength()
+    {
+        return this.menu.length;
+    }
+
+    /**
+     * Get first element in menu
+     *
+     * @returns {*}
+     */
+    getFirstElement()
+    {
+        return this.menu[0];
+    }
+
+    /**
+     * Get last element in menu
+     *
+     * @returns {*}
+     */
+    getLastElement()
+    {
+        return this.menu[this.getLength() - 1];
+    }
+
+    /**
+     * Initialization of the menu
+     */
+    init()
+    {
+        this.menu.forEach(item => {
+            item.setAttributes(); // Recursively set attributes
+        });
+    }
+
+}
