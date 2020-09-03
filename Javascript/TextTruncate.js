cfe46c3c11babf6ef6957721bdbe5daacf4326c4
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2020-09-03 15:05:06 +0200

    Update TextTruncate.js

--- Javascript/TextTruncate.js
+++ Javascript/TextTruncate.js
@@ -1,152 +1,158 @@
 /**
  * Truncate a text multilines
  */
-export default class TextTruncate
-{
+export default class TextTruncate {
 
-    /**
-     * @param $el
-     * @param wordLimit
-     * @param settings
-     */
-    constructor($el, wordLimit, settings)
-    {
-        this.$el = $el;
-        this.wordLimit = wordLimit;
-        this.settings = {
-            showMoreTextAttribute: 'data-show-more-text',
-            showLessTextAttribute: 'data-show-less-text',
-            showMoreButton: true,
-            showMoreButtonClass: 'showMore',
-            overflow: ' ...'
-        };
-        this._isTruncated = false;
-        this._setupSettings(settings);
+	/**
+	 * @param $el
+	 * @param wordLimit
+	 * @param settings
+	 */
+	constructor($el, wordLimit, settings) {
+		this.$el = $el;
+		this.wordLimit = wordLimit;
+		this.settings = {
+			showMoreTextAttribute: 'data-show-more-text',
+			showLessTextAttribute: 'data-show-less-text',
+			showMoreButton: true,
+			showMoreButtonClass: 'showMore',
+			overflow: ' ...'
+		};
+		this._isTruncated = false;
+		this._setupSettings(settings);
 
-        if (this._hasShowMore()) {
-            this.showMoreText = this.$el.getAttribute(this.settings.showMoreTextAttribute);
-            this.showLessText = this.$el.getAttribute(this.settings.showLessTextAttribute);
+		if (this._hasShowMore()) {
+			this.showMoreText = this.$el.getAttribute(this.settings.showMoreTextAttribute);
+			this.showLessText = this.$el.getAttribute(this.settings.showLessTextAttribute);
 
-            if (!this.showMoreText && this.settings.showMoreTextAttribute !== '') throw new Error(`You need to set the attribute ${this.settings.showMoreTextAttribute} for the text.`);
-            if (!this.showLessText && this.settings.showLessTextAttribute !== '') throw new Error(`You need to set the attribute ${this.settings.showLessTextAttribute} for the text.`);
-        }
+			if (!this.showMoreText
+			    && this.settings.showMoreTextAttribute
+			    !== '') throw new Error(`You need to set the attribute ${this.settings.showMoreTextAttribute} for the text.`);
+			if (!this.showLessText
+			    && this.settings.showLessTextAttribute
+			    !== '') throw new Error(`You need to set the attribute ${this.settings.showLessTextAttribute} for the text.`);
+		}
 
-        this.init();
-    }
+		this.init();
+	}
 
-    /**
-     * Set up settings defined in the creation of the instance
-     *
-     * @param settings
-     * @private
-     */
-    _setupSettings(settings)
-    {
-        if (Object.keys(settings).length > 0) {
-            for (let key in settings) {
-                if (settings.hasOwnProperty(key) && this.settings.hasOwnProperty(key)) this.settings[key] = settings[key];
-            }
-        }
-    }
+	/**
+	 * Set up settings defined in the creation of the instance
+	 *
+	 * @param settings
+	 * @private
+	 */
+	_setupSettings(settings) {
+		if (Object.keys(settings).length > 0) {
+			for (let key in settings) {
+				if (settings.hasOwnProperty(key) && this.settings.hasOwnProperty(key)) this.settings[key] = settings[key];
+			}
+		}
+	}
 
-    /**
-     * Has show more button
-     *
-     * @returns {boolean}
-     * @private
-     */
-    _hasShowMore()
-    {
-        return this.settings.showMoreButton;
-    }
+	/**
+	 * Has show more button
+	 *
+	 * @returns {boolean}
+	 * @private
+	 */
+	_hasShowMore() {
+		return this.settings.showMoreButton;
+	}
 
-    /**
-     * Is the text truncated
-     *
-     * @returns {boolean}
-     */
-    isTruncated()
-    {
-        return this._isTruncated;
-    }
+	/**
+	 * Is the text truncated
+	 *
+	 * @returns {boolean}
+	 */
+	isTruncated() {
+		return this._isTruncated;
+	}
 
-    /**
-     * Show truncated text
-     */
-    showTruncatedText()
-    {
-        const truncatedText = this.$el.getAttribute('data-truncated-text');
-        const $showMoreButton = this._hasShowMore() ? this.$el.querySelector('button') : null;
-        const $span = this.$el.querySelector('span');
+	/**
+	 * Show truncated text
+	 */
+	showTruncatedText() {
+		const truncatedText = this.$el.getAttribute('data-truncated-text');
+		const $showMoreButton = this._hasShowMore() ? this.$el.querySelector('button') : null;
+		const $span = this.$el.querySelector('span');
 
-        this.$el.setAttribute('data-is-truncated', 'false');
-        $span.textContent = ` ${truncatedText}`;
-        if (this._hasShowMore()) $showMoreButton.textContent = this.showLessText;
-        $span.setAttribute('tabindex', '0');
-        $span.focus();
-    }
+		this.$el.setAttribute('data-is-truncated', 'false');
+		$span.textContent = ` ${truncatedText}`;
+		if (this._hasShowMore()) $showMoreButton.textContent = this.showLessText;
+		$span.setAttribute('tabindex', '0');
+		$span.focus();
+		this.$el.dispatchEvent(new Event('show-truncated-text'));
+	}
 
-    /**
-     * Hide truncated text
-     */
-    hideTruncatedText()
-    {
-        const $showMoreButton = this._hasShowMore() ? this.$el.querySelector('button') : null;
-        const $span = this.$el.querySelector('span');
+	/**
+	 * Hide truncated text
+	 */
+	hideTruncatedText() {
+		const $showMoreButton = this._hasShowMore() ? this.$el.querySelector('button') : null;
+		const $span = this.$el.querySelector('span');
 
-        this.$el.setAttribute('data-is-truncated', 'true');
-        $span.textContent = this.settings.overflow;
-        if (this._hasShowMore()) $showMoreButton.textContent = this.showMoreText;
-        $span.setAttribute('tabindex', '-1');
-    }
+		this.$el.setAttribute('data-is-truncated', 'true');
+		$span.textContent = this.settings.overflow;
+		if (this._hasShowMore()) $showMoreButton.textContent = this.showMoreText;
+		$span.setAttribute('tabindex', '-1');
 
-    /**
-     * Create show more button
-     *
-     * @returns {HTMLButtonElement}
-     * @private
-     */
-    _createShowMoreButton()
-    {
-        const $showMoreButton = document.createElement('button');
-        $showMoreButton.textContent = this.showMoreText;
-        if (this.settings.showMoreButtonClass) $showMoreButton.classList.add(this.settings.showMoreButtonClass);
+		this.$el.dispatchEvent(new Event('hide-truncated-text'));
+	}
 
-        $showMoreButton.onclick = e => {
-            e.preventDefault();
+	/**
+	 * Create show more button
+	 *
+	 * @returns {HTMLButtonElement}
+	 * @private
+	 */
+	_createShowMoreButton() {
+		const $showMoreButton = document.createElement('button');
+		$showMoreButton.textContent = this.showMoreText;
+		if (this.settings.showMoreButtonClass) $showMoreButton.classList.add(this.settings.showMoreButtonClass);
 
-            const isTruncated = this.$el.getAttribute('data-is-truncated').toLowerCase() === 'true';
+		$showMoreButton.onclick = e => {
+			e.preventDefault();
 
-            if (isTruncated) {
-                this.showTruncatedText();
-            } else {
-                this.hideTruncatedText();
-            }
-        };
+			const isTruncated = this.$el.getAttribute('data-is-truncated').toLowerCase() === 'true';
 
-        return $showMoreButton;
-    }
+			if (isTruncated) {
+				this.showTruncatedText();
+			} else {
+				this.hideTruncatedText();
+			}
+		};
 
-    /**
-     * Init the text truncation
-     */
-    init()
-    {
-        let content = (this.$el.textContent.trim()).split(' ');
-        if (content.length < this.wordLimit) return;
+		return $showMoreButton;
+	}
 
-        this._isTruncated = true;
-        this.$el.setAttribute('data-is-truncated', 'true');
-        this.$el.setAttribute('data-truncated-text', (content.slice(this.wordLimit, content.length)).join(' '));
-        content = content.slice(0, this.wordLimit);
-        content = content.join(' ');
+	/**
+	 * Init the text truncation
+	 */
+	init() {
+		let content = (
+			this.$el.textContent.trim()
+		).split(' ');
+		if (content.length < this.wordLimit) return;
 
-        const $overflow = document.createElement('span');
-        $overflow.textContent = this.settings.overflow;
+		this._isTruncated = true;
+		this.$el.setAttribute('data-is-truncated', 'true');
+		this.$el.setAttribute(
+			'data-truncated-text',
+			(
+				content.slice(this.wordLimit, content.length)
+			).join(' ')
+		);
+		content = content.slice(0, this.wordLimit);
+		content = content.join(' ');
 
-        this.$el.textContent = content;
-        this.$el.appendChild($overflow);
-        if (this._hasShowMore()) this.$el.appendChild(this._createShowMoreButton());
-    }
+		const $overflow = document.createElement('span');
+		$overflow.textContent = this.settings.overflow;
+
+		this.$el.textContent = content;
+		this.$el.appendChild($overflow);
+		if (this._hasShowMore()) this.$el.appendChild(this._createShowMoreButton());
+		this.$el.dispatchEvent(new Event('init'));
+	}
 
 }
