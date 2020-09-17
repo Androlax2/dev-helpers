78cb96de86bdfe8f5695263acfff35955cced405
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2020-09-17 13:14:54 +0200

    Update Dropdown.js

--- Javascript/Menu/Dropdown.js
+++ Javascript/Menu/Dropdown.js
@@ -1,104 +1,96 @@
 /**
  * Base dropdown class
  */
-export default class Dropdown
-{
+export default class Dropdown {
 
-    constructor($dropdown)
-    {
-        if (!$dropdown) throw new Error('$dropdown parameter can\'t be null.');
+	constructor($dropdown) {
+		if (!$dropdown) throw new Error('$dropdown parameter can\'t be null.');
 
-        this.$menu = $dropdown;
-        this.isOpened = false;
-        this.menu = [];
-    }
+		this.$menu = $dropdown;
+		this.isOpened = false;
+		this.menu = [];
+	}
 
-    /**
-     * Set attributes
-     */
-    setAttributes()
-    {
-        this.getMenu().setAttribute('role', 'menu');
-        this.getMenu().setAttribute('aria-hidden', 'true');
-        this.menu.forEach(menuItemInSubMenu => menuItemInSubMenu.getToggler().setAttribute('tabindex', '-1'));
-        this.menu.forEach(item => item.setAttributes());
-    }
+	/**
+	 * Set attributes
+	 */
+	setAttributes() {
+		this.getMenu().setAttribute('role', 'menu');
+		this.getMenu().setAttribute('aria-hidden', 'true');
+		this.menu.forEach(menuItemInSubMenu => menuItemInSubMenu.getToggler().setAttribute('tabindex', '-1'));
+		this.menu.forEach(item => item.setAttributes());
+	}
 
-    /**
-     * Is dropdown open
-     *
-     * @returns {any}
-     */
-    isOpen()
-    {
-        return this.isOpened;
-    }
+	/**
+	 * Is dropdown open
+	 *
+	 * @returns {any}
+	 */
+	isOpen() {
+		return this.isOpened;
+	}
 
-    /**
-     * Get menu
-     *
-     * @returns {*}
-     */
-    getMenu()
-    {
-        return this.$menu;
-    }
+	/**
+	 * Get menu
+	 *
+	 * @returns {*}
+	 */
+	getMenu() {
+		return this.$menu;
+	}
 
-    /**
-     * Get Menu length
-     *
-     * @returns {*}
-     */
-    getMenuLength()
-    {
-        return this.menu.length;
-    }
+	/**
+	 * Get Menu length
+	 *
+	 * @returns {*}
+	 */
+	getMenuLength() {
+		return this.menu.length;
+	}
 
-    /**
-     * Get first element in sub menu
-     *
-     * @returns {{el: (*|Element|SVGAElement|HTMLAnchorElement), position: number}}
-     */
-    getFirstElement()
-    {
-        return {
-            el: this.menu[0].getToggler(),
-            position: 0
-        };
-    }
+	/**
+	 * Get first element in sub menu
+	 *
+	 * @returns {{el: (*|Element|SVGAElement|HTMLAnchorElement), position: number}}
+	 */
+	getFirstElement() {
+		return {
+			el: this.menu[0].getToggler(),
+			position: 0
+		};
+	}
 
-    /**
-     * Get last element in sub menu
-     *
-     * @returns {{el: (*|Element|SVGAElement|HTMLAnchorElement), position: number}}
-     */
-    getLastElement()
-    {
-        const position = this.getMenuLength() - 1;
-        return {
-            el: this.menu[position].getToggler(),
-            position: position
-        };
-    }
+	/**
+	 * Get last element in sub menu
+	 *
+	 * @returns {{el: (*|Element|SVGAElement|HTMLAnchorElement), position: number}}
+	 */
+	getLastElement() {
+		const position = this.getMenuLength() - 1;
+		return {
+			el: this.menu[position].getToggler(),
+			position: position
+		};
+	}
 
-    /**
-     * Open the dropdown
-     */
-    open()
-    {
-        this.getMenu().setAttribute('aria-hidden', 'false');
-        this.menu.forEach(menuItemInSubMenu => menuItemInSubMenu.getToggler().setAttribute('tabindex', '0'));
-        this.isOpened = true;
-    }
+	/**
+	 * Open the dropdown
+	 */
+	open() {
+		this.getMenu().setAttribute('aria-hidden', 'false');
+		this.menu.forEach(menuItemInSubMenu => menuItemInSubMenu.getToggler().setAttribute('tabindex', '0'));
+		this.isOpened = true;
+		this.$menu.dispatchEvent(new Event('dropdownOpen'));
+	}
 
-    /**
-     * Close the dropdown
-     */
-    close()
-    {
-        this.getMenu().setAttribute('aria-hidden', 'true');
-        this.menu.forEach(menuItemInSubMenu => menuItemInSubMenu.getToggler().setAttribute('tabindex', '-1'));
-        this.isOpened = false;
-    }
+	/**
+	 * Close the dropdown
+	 */
+	close() {
+		this.getMenu().setAttribute('aria-hidden', 'true');
+		this.menu.forEach(menuItemInSubMenu => menuItemInSubMenu.getToggler().setAttribute('tabindex', '-1'));
+		this.isOpened = false;
+		this.$menu.dispatchEvent(new Event('dropdownClose'));
+	}
 
 }
