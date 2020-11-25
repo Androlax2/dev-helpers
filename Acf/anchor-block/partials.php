ba1850859dd108996771604535ee6240ba0f0252
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2020-11-25 16:14:43 +0100

    Update partials.php

--- Acf/anchor-block/partials.php
+++ Acf/anchor-block/partials.php
@@ -6,45 +6,65 @@ use StoutLogic\AcfBuilder\FieldsBuilder;
 // ANCHOR BLOCK
 //======================================================================
 
-$anchorBlock = new FieldsBuilder('anchor-block', [
-	'label' => 'Ancres'
-]);
+//@formatter:off
+        $anchorBlock = new FieldsBuilder('anchor_block', [
+        	'title' => 'Ancres'
+        ]);
 
-$anchorBlock
-	->addRepeater('blocks', [
-		'required' => true,
-		'label' => 'Blocs',
-		'min' => 1,
-		'button_label' => 'Ajouter un bloc d\'ancre'
-	])
-		->addRepeater('inner_block', [
-			'required' => true,
-			'label' => 'Contenu du bloc',
-			'min' => 1,
-			'button_label' => 'Ajouter un contenu au bloc',
-			'layout' => 'block'
-		])
-			->addTrueFalse('different_content_title', [
-				'label' => 'Titre du contenu différent ?',
-				'instructions' => 'Permet d\'utiliser un titre différent',
-				'ui' => 1
-			])
-				->addWysiwyg('content_title', [
-					'required' => true,
-					'label' => 'Titre du contenu',
-					'media_upload' => false
-				])
-					->conditional('different_content_title', '==', '1')
-			->addWysiwyg('title', [
-				'required' => true,
-				'label' => 'Titre',
-				'media_upload' => false
-			])
-			->addWysiwyg('text', [
-				'required' => true,
-				'label' => 'Texte'
-			])
-		->endRepeater()
-	->endRepeater();
+        $anchorBlock
+	        ->addRepeater('blocks', [
+		        'required' => true,
+		        'label' => 'Blocs',
+		        'min' => 1,
+		        'button_label' => 'Ajouter un bloc d\'ancre'
+	        ])
+		        ->addRepeater('inner_block', [
+			        'required' => true,
+			        'label' => 'Contenu du bloc',
+			        'min' => 1,
+			        'button_label' => 'Ajouter un contenu au bloc',
+			        'layout' => 'block'
+		        ])
+	                ->addTrueFalse('automatic_anchor_name', [
+	                	'label' => 'Automatiser le nom de l\'ancre ? (Dans l\'URL)',
+		                'default_value' => 1
+	                ])
+	                ->addText('anchor_name', [
+	                	'label' => 'Nom de l\'ancre',
+		                'instructions' => 'Le texte va automatiquement être transformé pour être adapté à l\'url'
+	                ])
+	                    ->conditional('automatic_anchor_name', '==', '0')
+			        ->addTrueFalse('different_content_title', [
+				        'label' => 'Titre du contenu différent ?',
+				        'instructions' => 'Permet d\'utiliser un titre différent',
+			        ])
+			        ->addWysiwyg('content_title', [
+				        'required' => true,
+				        'label' => 'Titre du contenu',
+				        'media_upload' => false
+			        ])
+			            ->conditional('different_content_title', '==', '1')
+			        ->addWysiwyg('title', [
+				        'required' => true,
+				        'label' => 'Titre',
+				        'media_upload' => false
+			        ])
+	                ->addFlexibleContent('content', [
+	                	'required' => true,
+		                'label' => 'Contenu',
+		                'button_label' => 'Ajouter un contenu'
+	                ])
+	                    ->addLayout('text', [
+	                    	'title' => 'Texte'
+	                    ])
+					        ->addWysiwyg('text', [
+						        'required' => true,
+						        'label' => 'Texte'
+					        ])
+	                    ->addLayout($this->get(Video::class))
+	                ->endFlexibleContent()
+	            ->endRepeater()
+	        ->endRepeater();
+        //@formatter:on
 
 return $anchorBlock;
