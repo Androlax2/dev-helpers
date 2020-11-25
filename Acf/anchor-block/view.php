1d19a806dec22aabdb555efb087614383e4d55fe
Androlax2 <39646949+Androlax2@users.noreply.github.com>
2020-11-25 16:14:07 +0100

    Update view.php

--- Acf/anchor-block/view.php
+++ Acf/anchor-block/view.php
@@ -1,54 +1,72 @@
 <?php
-	$slugify = new \Cocur\Slugify\Slugify();
-	$blocks = get_sub_field('blocks');
+
+$slugify = new \Cocur\Slugify\Slugify();
+$blocks = get_sub_field('blocks');
+
 ?>
 
-<div class="anchorBlock">
-	<div class="anchorBlock__links" aria-hidden="true">
-		<ul>
-			<?php foreach ($blocks as $block): ?>
-				<?php $numberOfBlocks = count($block['inner_block']); ?>
-				<li>
-					<?php foreach ($block['inner_block'] as $index => $innerBlock): ?>
-						<?php
-						$title = strip_tags($innerBlock['title']);
+<?php if (have_rows('blocks')): ?>
+    <div class="anchorBlock">
+        <div class="anchorBlock__links" aria-hidden="true">
+            <ul>
+				<?php foreach ($blocks as $block): ?>
+					<?php $numberOfBlocks = count($block['inner_block']); ?>
+                    <li>
+						<?php foreach ($block['inner_block'] as $index => $innerBlock): ?>
+							<?php
+							$slug = (!$innerBlock['automatic_anchor_name'] ? strip_tags($innerBlock['anchor_name']) : strip_tags($innerBlock['title']));
+							$title = strip_tags($innerBlock['title']);
 
-						// Wrap "?" in a span to avoid it wrapping alone
-						if (preg_match('/\?$/', $title)) {
-							$title = rtrim(preg_replace('/\?$/', '', $title));
-							$title .= '<span>?</span>';
-						}
-						?>
-						<?php if ($index === 0): ?>
-							<a href="#<?php echo $slugify->slugify(strip_tags($innerBlock['title'])); ?>"><?php echo $title; ?></a>
-						<?php endif; ?>
-						<?php if ($numberOfBlocks > 1 && $index === 1): ?>
-							<ul>
-						<?php endif; ?>
-						<?php if ($index > 0): ?>
-							<li>
-								<a href="#<?php echo $slugify->slugify(strip_tags($innerBlock['title'])); ?>"><?php echo $title; ?></a>
-							</li>
-						<?php endif; ?>
-						<?php if ($numberOfBlocks > 1 && ($index === ($numberOfBlocks - 1))): ?>
-							</ul>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
-    <div class="anchorBlock__border"></div>
-	<div class="anchorBlock__content">
-		<?php foreach ($blocks as $block): ?>
-			<div class="anchorBlock__content__block">
-				<?php foreach ($block['inner_block'] as $innerBlock): ?>
-					<div id="<?php echo $slugify->slugify(strip_tags($innerBlock['title'])); ?>" class="anchorBlock__content__block__inner applyStyles">
-						<?php echo !$innerBlock['different_content_title'] ? $innerBlock['title'] : $innerBlock['content_title']; ?>
-						<?php echo $innerBlock['text']; ?>
-					</div>
+							// Wrap "?" in a span to avoid it wrapping alone
+							if (preg_match('/\?$/', $title)) {
+								$title = rtrim(preg_replace('/\?$/', '', $title));
+								$title .= '<span>?</span>';
+							}
+							?>
+							<?php if ($index === 0): ?>
+                                <a href="#<?php echo $slugify->slugify($slug); ?>"><?php echo $title; ?></a>
+							<?php endif; ?>
+							<?php if ($numberOfBlocks > 1 && $index === 1): ?>
+                                <ul>
+							<?php endif; ?>
+							<?php if ($index > 0): ?>
+                                <li>
+                                    <a href="#<?php echo $slugify->slugify($slug); ?>"><?php echo $title; ?></a>
+                                </li>
+							<?php endif; ?>
+							<?php if ($numberOfBlocks > 1 && ($index === ($numberOfBlocks - 1))): ?>
+                                </ul>
+							<?php endif; ?>
+						<?php endforeach; ?>
+                    </li>
 				<?php endforeach; ?>
-			</div>
-		<?php endforeach; ?>
-	</div>
-</div>
+            </ul>
+            <div class="anchorBlock__links__line"></div>
+        </div>
+        <div class="anchorBlock__content">
+			<?php while (have_rows('blocks')): the_row(); ?>
+                <div class="anchorBlock__content__block">
+					<?php while (have_rows('inner_block')): the_row(); ?>
+						<?php
+						//@formatter:off
+                        $slug = (!get_sub_field('automatic_anchor_name') ? strip_tags(get_sub_field('anchor_name')) : strip_tags(get_sub_field('title')));
+                        //@formatter:on
+						?>
+                        <div id="<?php echo $slugify->slugify($slug); ?>" class="anchorBlock__content__block__inner applyStyles">
+							<?php echo !get_sub_field('different_content_title') ? get_sub_field('title') : get_sub_field('content_title'); ?>
+							<?php if (have_rows('content')): ?>
+								<?php while (have_rows('content')): the_row(); ?>
+									<?php if (get_row_layout() === 'text'): ?>
+										<?php echo get_sub_field('text'); ?>
+									<?php else: ?>
+										<?php get_template_part('resources/views/' . str_replace('_', '-', get_row_layout())); ?>
+									<?php endif; ?>
+								<?php endwhile; ?>
+							<?php endif; ?>
+                        </div>
+					<?php endwhile; ?>
+                </div>
+			<?php endwhile; ?>
+        </div>
+    </div>
+<?php endif; ?>
